64d5c0a444a125359eec07d0ec579dc5c3f85436
elenaJO <ele.lucero1993@gmail.com>
2017-11-19 17:36:38 -0500

    añadiendo eslintrc , cometarios y estilos

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 var subMenu = document.getElementsByClassName('submenu');
-for(var i=0;i<subMenu.length;i++){
+for (var i = 0;i < subMenu.length;i++) {
   subMenu[i].addEventListener('click',showMenu);
 }
 
-function showMenu(){
-    var listMenu=this.getElementsByClassName('itemList')[0];
-    if(listMenu.classList.contains('hide')){
-      listMenu.classList.remove('hide');
-      listMenu.classList.add('show');
-    } else {
-      listMenu.classList.remove('show');
-      listMenu.classList.add('hide'); 
-    }
+function showMenu() {
+  var listMenu = this.getElementsByClassName('itemList')[0];
+  if (listMenu.classList.contains('hide')) {
+    listMenu.classList.remove('hide');
+    listMenu.classList.add('show');
+  } else {
+    listMenu.classList.remove('show');
+    listMenu.classList.add('hide'); 
+  }
 } 
